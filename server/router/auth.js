81c7e97122376058f1eff1c1f0eb900eeb99ce21
Aniket Kumar <aniketskr@gmail.com>
2023-05-20 00:56:55 +0530

    connected MongoDB

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+require("../db/conn");
+const User = require("../model/userSchema");
+
 router.get("/", (req, res) => {
   res.send("Home from auth!");
 });
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
+  const { name, email, phone, work, password, cpassword } = req.body;
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res
+      .status(402)
+      .json({ error: "PLease filled the fields properly!" });
+  }
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "User already exist" });
+      }
+      const user = new User({ name, email, phone, work, password, cpassword });
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: "User registered successfully" });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: "Failed to register" });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
